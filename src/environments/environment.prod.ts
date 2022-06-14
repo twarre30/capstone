1a24cf4f5979160a24b3c3fa63ef311c59a3ec5d
Tiffany Warren <twarre30@gmail.com>
2022-06-13 20:13:04 -0500

    one more api key update to show on netlify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,3 +2,7 @@ export const environment = {
   production: true,
   baseApiUrl: 'https://member-directory-api.herokuapp.com',
 };
+
+export const apiKeyConfig = {
+  weatherApiKey: 'https://api.openweathermap.org/data/2.5/weather?lat=35.798670&lon=-86.907341&appid=1f3fb906496b5ab8d9a99ae6e017557e&units=imperial',
+}
