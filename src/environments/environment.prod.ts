df6836faa83995d4e6c78bd83d5e41c6246a5120
Tiffany Warren <twarre30@gmail.com>
2022-06-16 12:01:59 -0500

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,4 @@ export const environment = {
   baseApiUrl: 'https://member-directory-api.herokuapp.com',
 };
 
-export const apiKeyConfig = {
-  weatherApiKey: 'https://api.openweathermap.org/data/2.5/weather?lat=35.925064&lon=-86.868889&appid=1f3fb906496b5ab8d9a99ae6e017557e&units=imperial',
-}
+
